f8b2b2a432773979dcef80af12481954e46069fd
IrynnnaFedorova <bulhakovairynnna@gmail.com>
2022-08-30 23:17:55 +0300

    Визуал входа и отображение иконки юзера в хедере

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import {MainView} from "../views/MainView";
+import {Header} from './Header/Header';
+import '../CSS/normilize.css';
+import '../CSS/fonts.css';
+
+export const App = () => {
+  return (
+      <>
+        <Header/>
+        <MainView/>
+      </>
+
+
+  );
+};
+
