74663f376eb99a021a1dd4e69f71f394d03220f3
Andrey-Baranyuk <andreybaranyuk86@gmail.com>
2022-09-01 11:15:24 +0300

    1

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import { ReactComponent as Diagram } from "../../images/icons/Diagram.svg";
 import s from "./Balance.module.scss";
 
