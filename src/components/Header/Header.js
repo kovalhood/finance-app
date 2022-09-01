ff82519ef09338845bc1c76e57e7179a643008e8
IrynnnaFedorova <bulhakovairynnna@gmail.com>
2022-09-01 12:17:38 +0300

    Header Exit,avatar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,52 +4,43 @@ import styles from "./Header.module.css";
 import { Logo } from "../Logo/Logo";
 import { ReactComponent as Logout } from './icons/logout.svg';
 import defaultAvatar from './icons/user.png';
-import Button from "react-bootstrap/Button";
-import { useSelector } from 'react-redux';
 import React, { useState } from 'react';
 import Modal from '../Modal';
-import authSelectors from '../../redux/selectors/auth-selectors';
+
 
   export const Header = () => {
-  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
-  const userEmail = useSelector(authSelectors.getUserEmail);
-  const userPicture = useSelector(authSelectors.getUserPicture);
   const [showModal, setShowModal] = useState(false);
 
-  const toggleModal = () => {
+ const toggleModal = e => {
+    e.preventDefault();
     setShowModal(prevShowModal => !prevShowModal);
   };
 
     return (
-     <div
-        className={
-          isLoggedIn ? styles.backgroundLoggedIn : styles.backgroundLoggedOut
-        }>
+    <div>
     <div className={styles.wrapper}>
-    <header className={styles.header}>
-         <Logo />
-        <div className={styles.user__menu}>
-                <img
-                  src={userPicture ? userPicture : defaultAvatar}
-                  alt="user avatar"
-            className={styles.user__avatar} />
-          <Button onClick={toggleModal}  className={styles.logout__mobile}
-            variant="primary"
-            type ="submit">
-                  <Logout />
-                </Button>
-                <span type="button" className={styles.user__name}>{userEmail}</span>
-                <a href="/" onClick={toggleModal} className={styles.logout}> Выйти </a>
-              </div>
-         </header>
-       { showModal && (
+    <header>
+      <Logo />
+      <div className={styles.user__menu}>
+      <img src={defaultAvatar}
+      alt="user avatar"
+      className={styles.user__avatar} />
+      <button type="button" className={styles.button_logout}onClick={toggleModal} >
+      <Logout />
+      </button>
+            <span className={styles.user__name}></span>
+      <a href="/" className={styles.button_logout} onClick={toggleModal}> Exit</a></div>
+      </header>
+    </div>
+    {showModal && (
           <Modal
-            closeModal={toggleModal}
-            massage={'Do you really want to leave?'}
+            onClick={toggleModal}
+            massage={'Do you really want to log out?'}
           />
-        )}
-        </div>
-        </div>
-   );
-};
+  )
+  }
+  </div>
+  )
+}
 
+export default Header;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,47 @@
-import styles from "./Header.module.css";
-import { useMediaQuery } from "react-responsive";
-import { isDesktop } from "../../utils/mediaQuery";
-import { Logo } from "../Logo/Logo";
+import styles from './Header.module.scss'
+import React, { useState } from 'react';
+import {useMediaQuery} from 'react-responsive';
+import { isDesktop} from '../../utils/mediaQuery';
+import { Logo } from "../Logo/Logo"; 
+import { ReactComponent as Logout } from './icons/logout.svg';
+import defaultAvatar from './icons/user.png';
+import { useSelector } from 'react-redux';
+import Modal from '../Modal';
 
 export const Header = () => {
   const Desktop = isDesktop(useMediaQuery);
+  const [showModal, setShowModal] = useState(false);
+
+ const toggleModal = e => {
+    e.preventDefault();
+    setShowModal(prevShowModal => !prevShowModal);
+  };
 
   return (
+    <div>
+    <div className={styles.wrapper}>
     <header className={Desktop ? styles.DesktopHeader : styles.TabletHeader}>
       <Logo />
-      
-    </header>
-  );
-};
+      <div className={styles.user__menu}>
+      <img src={defaultAvatar}
+      alt="user avatar"
+      className={styles.user__avatar} />
+      <button type="button" className={styles.button_logout}onClick={toggleModal} >
+      <Logout />
+      </button>
+            <span className={styles.user__name}></span>
+      <a href="/" className={styles.button_logout} onClick={toggleModal}> Exit</a></div>
+      </header>
+    </div>
+    {showModal && (
+          <Modal
+            onClick={toggleModal}
+            massage={'Do you really want to log out?'}
+          />
+  )
+  }
+  </div>
+  )
+}
+
+export default Header;
