05256dfebdded5209b54f93e210a6e6756b2e058
IrynnnaFedorova <bulhakovairynnna@gmail.com>
2022-09-02 19:09:03 +0300

    Modal, user menu header, redux,

--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -1,11 +1,14 @@
 const getIsLoggedIn = (state) => state.auth.isLoggedIn;
 
-const getUsername = (state) => state.auth.user.email;
+const getEmail = (state) => state.auth.user.email;
+const getName = (state) => state.auth.user.name;
 const getIsFetchingCurrentUser = (state) => state.auth.isFetchingCurrentUser;
-
+const getUserPicture = (state) => state.auth.user.picture;
 const authSelectors = {
   getIsLoggedIn,
-  getUsername,
+  getUserPicture,
+  getEmail,
+  getName,
   getIsFetchingCurrentUser,
 };
 export default authSelectors;
