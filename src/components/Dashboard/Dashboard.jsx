0047e4d5c3ccc2acdf5fcb856bf6542a7ab79f23
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-09-05 13:14:46 +0300

    Added basic logic

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,17 +1,19 @@
-import Controls from "./Controls";
-import Form from "./Form";
-import styles from "./Dashboard.module.scss";
+import Controls from './Controls';
+import Form from './Form';
+import styles from './Dashboard.module.scss';
 
 export const Dashboard = () => {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.controls}>
-          <Controls link={"/expense"} title={"Expense"} />
-          <Controls link={"/income"} title={"Income"} />
-        </div>
-        <div className={styles.board}>
-          <Form />
+        <div className={styles.dashboard}>
+          <div className={styles.controls}>
+            <Controls link={'/expense'} title={'Expense'} />
+            <Controls link={'/income'} title={'Income'} />
+          </div>
+          <div className={styles.board}>
+            <Form />
+          </div>
         </div>
       </div>
     </>
