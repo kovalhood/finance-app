589e3dbbaffab8d7a9e15f362f70001e09915700
Vasylij-Kashtalyan <88617002+Vasylij-Kashtalyan@users.noreply.github.com>
2022-09-06 22:03:13 +0300

    add sumSlice

--- src/components/SummaryTrans/SummaryTrans.jsx
+++ src/components/SummaryTrans/SummaryTrans.jsx
@@ -5,35 +5,40 @@ import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import operations from '../../redux/operation/authOperations';
 import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import summarySelectors from '../../redux/summary/summarySelector';
 
 export const SummaryTrans = () => {
   const Desktop = isDesktop(useMediaQuery);
   const Table = isTablet(useMediaQuery);
+  const getMonth = (new Date().getMonth() + 1).toString().padStart(2, '0');
 
   const dispatch = useDispatch();
   const { type } = useParams();
-  console.log(type);
 
   useEffect(() => {
     dispatch(operations.getTransactionsByMonth(type));
   }, [dispatch, type]);
 
+  const mounth = useSelector(summarySelectors.getSummary);
+
   return (
     <div className={Desktop || Table ? styles.container : styles.none}>
       <div className={styles.titleBlock}>
-        <h3>
-          Сводка
-          <p>{}</p>
-        </h3>
+        <h3>Сводка</h3>
       </div>
 
       <ul>
-        {/* <li key={""} className={styles.monthPoint}>
-          <p className={styles.monthPointText}>{""}</p>
-          <p className={styles.monthPointText}>{""}</p>
-        </li> */}
-
-        <div className={styles.list}> </div>
+        {mounth.lenght > 0 ? (
+          mounth.map(({ total, _id }) => (
+            <li key={_id} className={styles.monthPoint}>
+              <p className={styles.monthPointText}>{getMonth._id}</p>
+              <p className={styles.monthPointText}>{total}</p>
+            </li>
+          ))
+        ) : (
+          <div className={styles.list}> </div>
+        )}
       </ul>
     </div>
   );
