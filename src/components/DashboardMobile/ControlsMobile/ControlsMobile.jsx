50ef55dcdf173a6046f32cf60341a2f9d50eb9f5
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-09-07 13:44:49 +0300

    Added mobile setup

---
+++ src/components/DashboardMobile/ControlsMobile/ControlsMobile.jsx
@@ -0,0 +1,12 @@
+import { NavLink } from 'react-router-dom';
+import styles from './ControlsMobile.module.scss';
+
+const ControlsMobile = ({ link, title }) => {
+  return (
+    <div>
+      <p>Controls</p>
+    </div>
+  );
+};
+
+export default ControlsMobile;
---
+++ src/components/DashboardMobile/DashboardMobile.jsx
@@ -0,0 +1,15 @@
+import ControlsMobile from './ControlsMobile';
+import FormMobile from './FormMobile';
+import styles from './DashboardMobile.module.scss';
+
+export const DashboardMobile = () => {
+  console.log('mobile');
+  return (
+    <>
+      <div className={styles.container}>
+        <FormMobile />
+        <p>Test</p>
+      </div>
+    </>
+  );
+};
