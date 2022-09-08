64993a0b8414bf6ffe522816ce349c848acd0641
rumpear <roman.f.grusha@gmail.com>
2022-09-08 17:47:28 +0300

    added axisX, axisY

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -22,8 +22,11 @@ const data3 = [
 
 const Charts = ({ data }) => {
   const Mobile = isMobile(useMediaQuery);
-  // const keys = Object.keys(...data);
-  // console.log(keys[0], 'keys===keys');
+  const axisX = Object.keys(...data)[1];
+  const axisY = Object.keys(...data)[0];
+  console.log(axisX, 'axisX');
+  console.log(axisY, 'axisY');
+
   // console.log(data, 'data============data');
 
   // const values = data.map(item => item.sum);
@@ -51,12 +54,13 @@ const Charts = ({ data }) => {
         // horizontal
         dependentAxis
         // tickFormat specifies how ticks should be displayed
-        // tickFormat={x => `$${x / 1000}uah`}
+        // tickFormat={x => `${x} uah`}
+        // tickFormat={x => x + ' uah'}
       />
       <VictoryBar
         data={data}
-        x="category"
-        y="sum"
+        x={axisX}
+        y={axisY}
         style={{ data: { fill: 'tomato' } }}
       />
     </VictoryChart>
