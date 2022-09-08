3386d2f0959dd54f374ba2533b3405de21982b9e
AnnaKucherenko <annakucherenko31@gmail.com>
2022-09-08 09:52:39 +0300

    список для мобилки

---
+++ src/components/DashboardMobile/Tablemobile/TableMobile.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import operations from '../../../redux/operation/authOperations';
+
+import Transaction from './Transaction';
+import styles from './TableMobile.module.scss';
+
+export default function TableMobile({ date }) {
+  const [transactions, setTransactions] = useState([]);
+  const dispatch = useDispatch();
+
+  const dayQuery = new Date(date).getDate().toString().padStart(2, '0');
+  const monthQuery = (new Date(date).getMonth() + 1)
+    .toString()
+    .padStart(2, '0');
+  const yearQuery = new Date(date).getFullYear();
+
+  useEffect(() => {
+    dispatch(
+      operations.getAllTransactions.allTransactions({
+        dayQuery,
+        monthQuery,
+        yearQuery,
+      })
+    )
+      .then(res => {
+        const trans = res.payload;
+        setTransactions(trans);
+      })
+      .catch(err => console.log(err));
+  }, [dispatch, dayQuery, monthQuery, yearQuery]);
+
+  const trans = transactions.map(item => {
+    const day = item.day;
+    const month = item.month;
+    const year = item.year;
+    if (day === '' && month === '' && year === '') {
+      return;
+    } else {
+      const date = `${day}.${month}.${year}`;
+      item.date = date;
+    }
+  });
+  console.log(trans);
+
+  //   const arrayLength = trans.length;
+
+  //   function createTableOfNineRows(length) {
+  //     if (length >= 16) {
+  //       return;
+  //     }
+  //     if (length < 16) {
+  //       const id = nanoid();
+  //       transactions.push({
+  //         _id: `${id}`,
+  //         date: '',
+  //         day: '',
+  //         month: '',
+  //         year: '',
+  //         description: '',
+  //         categories: '',
+  //         value: null,
+  //         income: true,
+  //         // owner: '6315d0f27a7659ec61c4543f',
+  //       });
+  //       const newArrayLength = transactions.length;
+  //       createTableOfNineRows(newArrayLength);
+  //     }
+  //   }
+
+  //   createTableOfNineRows(arrayLength);
+  // // console.log(transactions);
+
+  return (
+    <>
+      <ul className={styles.transactionHistory}>
+        {trans.map(item => (
+          <Transaction
+            key={item._id}
+            id={item._id}
+            date={item.date}
+            description={item.description}
+            category={item.categories}
+            sum={item.value}
+            income={item.income}
+          />
+        ))}
+      </ul>
+    </>
+  );
+}
---
+++ src/components/DashboardMobile/Tablemobile/Transaction.jsx
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+import { RedactSvgSelector } from './RedactSvgSelector.jsx';
+import { useDispatch } from 'react-redux';
+import operation from '../../../redux/operation/authOperations';
+
+import styles from './TableMobile.module.scss';
+
+const Transaction = ({ id, date, description, category, sum, income }) => {
+  const dispatch = useDispatch();
+  const value = sum;
+  const isIncome = income;
+
+  if (value) {
+    return (
+      <li className={styles.transaction}>
+        <p className={styles.transaction_description}>{description}</p>
+        <div className={styles.flex}>
+          <p className={styles.transaction_date}>{date}</p>
+          <p className={styles.transaction_category}>{category}</p>
+        </div>
+        {isIncome && <li className={styles.green_color}>{sum}.00 uah</li>}
+        {!isIncome && <li className={styles.red_color}>-{sum}.00 uah</li>}
+
+        <button
+          type="button"
+          onClick={() => dispatch(operation.deleteTransaction({ id }))}
+          className={styles.button}
+        >
+          <RedactSvgSelector id="delete" />
+        </button>
+      </li>
+    );
+  }
+};
+
+Transaction.propTypes = {
+  id: PropTypes.string,
+  date: PropTypes.string,
+  description: PropTypes.string,
+  category: PropTypes.string,
+  sum: PropTypes.number,
+  income: PropTypes.bool,
+};
+
+export default Transaction;
