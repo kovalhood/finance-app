4ba8036abe878ebaaddc8a4db1aff9c57e7d774f
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-09-11 14:15:54 +0300

    Added null balance logic

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,3 +1,5 @@
+import { useSelector } from 'react-redux';
+import { authSelectors } from '../../redux/operation';
 import Controls from './Controls';
 import Form from './Form';
 import { HeaderTable } from './Table/HeaderTable';
@@ -6,9 +8,14 @@ import styles from './Dashboard.module.scss';
 import { Summary } from '../Summary';
 
 export const Dashboard = () => {
+  const getBalance = useSelector(authSelectors.getBalance);
+
   return (
     <>
-      <div className={styles.container}>
+      <div
+        className={styles.container}
+        style={getBalance === null ? { pointerEvents: 'none' } : {}}
+      >
         <div className={styles.dashboard}>
           <div className={styles.controls}>
             <Controls link={'/expense'} title={'Expense'} />
