591b85fb6b676e1f5275bca2a2979a3c521cf4dc
Roman <77742141+rumpear@users.noreply.github.com>
2022-09-13 00:38:07 +0300

    Charts upd

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -5,82 +5,54 @@ import {
   VictoryLabel,
   VictoryTheme,
 } from 'victory';
-import { useTranslation } from 'react-i18next';
 import { useMediaQuery } from 'react-responsive';
 import { isMobile } from '../../utils/mediaQuery';
-import { formatSum } from '../../utils';
 
 const CHARTS_STYLE = {
-  // data: {
-  //   fill: '#FF751D',
-  // },
   data: {
     width: 15,
     fill: '#FF751D',
   },
 };
 
+const LABEL_STYLE = {
+  fontFamily: 'Roboto',
+  fontSize: 9,
+  fill: '#52555F',
+};
+
+const CORNER_RADIUS = {
+  topLeft: 5,
+  topRight: 5,
+};
+
 const Charts = ({ data }) => {
-  const { t } = useTranslation();
   const mobile = isMobile(useMediaQuery);
   const [axisY, axisX] = Object.keys(...data);
-  const height = mobile ? 400 : 700;
-  const width = mobile ? 300 : 600;
-
-  const handleTickFormat = x => {
-    return `${formatSum(x)} ${t('hrn')}`;
-  };
+  const height = mobile ? 400 : 300;
+  const width = mobile ? 200 : 600;
 
   return (
-    // <>
-    //   <VictoryAxis horizontal={mobile} />
-    //   {mobile ? (
-    //     <VictoryAxis />
-    //   ) : (
-    //     <VictoryAxis dependentAxis tickFormat={handleTickFormat} />
-    //   )}
-
-    //   <VictoryBar data={data} x={axisX} y={axisY} style={CHARTS_STYLE} />
-    // </>
-
     <VictoryChart
-      domainPadding={40}
-      // padding={{ left: 90, top: 50, right: 10, bottom: 50 }}
+      domainPadding={10}
+      // padding={70}
       height={height}
       width={width}
       theme={VictoryTheme.material}
     >
-      <VictoryAxis horizontal={mobile} />
-      {mobile ? (
-        <VictoryAxis style={{ tickLabels: { angle: -60 } }} />
-      ) : (
-        <VictoryAxis dependentAxis tickFormat={handleTickFormat} />
-      )}
-
+      <VictoryAxis
+        horizontal={mobile}
+        style={{
+          tickLabels: LABEL_STYLE,
+        }}
+      />
       <VictoryBar
         data={data}
         x={axisX}
         y={axisY}
         style={CHARTS_STYLE}
-        cornerRadius={{ topLeft: 5, topRight: 5 }}
-        labelComponent={
-          <VictoryLabel
-            dy={-13}
-            dx={1}
-            style={[
-              {
-                // display: 'flex',
-                // justifyContent: 'center',
-                // alignItems: 'center',
-                // textAlign: 'center',
-                fontFamily: 'Roboto',
-                fontSize: 10,
-                lineHeight: 12,
-                fill: '#52555F',
-              },
-            ]}
-          />
-        }
+        cornerRadius={CORNER_RADIUS}
+        labelComponent={<VictoryLabel style={LABEL_STYLE} />}
       />
     </VictoryChart>
   );
