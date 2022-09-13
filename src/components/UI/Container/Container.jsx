c9f207f9296ab55c2945941b9962f382d3b1b197
Roman <77742141+rumpear@users.noreply.github.com>
2022-09-13 20:46:57 +0300

    Container refactoring

--- src/components/UI/Container/Container.jsx
+++ src/components/UI/Container/Container.jsx
@@ -5,15 +5,3 @@ const Container = ({ children }) => {
 };
 
 export default Container;
-
-// import styles from './Container.module.scss';
-
-// const Container = ({ children }) => {
-//   return (
-//     <div className={styles.background}>
-//       <div className={styles.layout}>{children}</div>
-//     </div>
-//   );
-// };
-
-// export default Container;
